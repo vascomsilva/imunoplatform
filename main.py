616be80ede34e5af988567de5da4fd1d18752b97
vascomsilva <vascofmsilva@gmail.com>
2020-08-05 00:15:00 +0100

    nada

--- main.py
+++ main.py
@@ -20,4 +20,3 @@ csv = df.to_csv(index=False)
 b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
 href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
 st.markdown(href, unsafe_allow_html=True)
->>>>>>> cf0fde6dd9d741d16c986e3f2cd792869205d958
